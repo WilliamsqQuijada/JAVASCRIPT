476dbdc969bd3070039337fa4a48a33d81f1f785
Williams Perez <quijadawilliams155@gmail.com>
2025-05-20 20:14:53 -0600

    Nuevos archivos

---
+++ Ejercicio_IF-01.js
@@ -0,0 +1,11 @@
+// EJERCICIO PARA SABER SI UNA PERSONA ES MAYOR DE EDAD O ES MENOR DE EDAD 
+
+const edad = 18;
+let age = 50;
+
+if (age >= edad){
+    console.log(`La edad de la persona es: ${age}, por lo tanto es mayor de edad`)
+}
+else{
+    console.log(`La edad de la persona es: ${age}, por lo tanto es menor de edad`)
+}
---
+++ Ejercicio_IF-02.js
@@ -0,0 +1,29 @@
+// EJERCICIO DEL DIA DE LA SEMANA CON ELSE IF
+
+// DECLARAMOS VARIABLE PARA HACER REFERENCIA AL DIA DE LA SEMANA 
+let dia = 6;
+
+if (dia == 1){
+    console.log("LUNES");
+}
+else if (dia == 2){
+    console.log("MARTES");
+}
+else if (dia == 3){
+    console.log("MIERCOLES");
+}
+else if (dia == 4){
+    console.log("JUEVES");
+}
+else if (dia == 5){
+    console.log("VIERNES");
+}
+else if (dia == 6){
+    console.log("SABADO");
+}
+else if (dia == 7){
+    console.log("DOMINGO");
+}
+else{
+    console.log("NO EXISTE TAL DIA");
+}
